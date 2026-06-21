949df4dd166d326e3b91457587cc82a67e552601
zimingwang <1379005338@qq.com>
2026-06-21 13:01:27 +0800

    refactor: replace #if with if constexpr in outlier_detection.cu
    - Merge 3 duplicate __intersection__cube definitions into one (if constexpr branches)
    - Replace #if OPTIMIZATION/#elif in __raygen__rg with if constexpr
    - Replace #if DIMENSION/#elif with if constexpr
    - Replace #if DEBUG_INFO with if constexpr
    - Preserve all optixTrace parameter comments
    
    203 -> 130 lines, 0 #if blocks remaining

--- src/ods/outlier_detection.cu
+++ src/ods/outlier_detection.cu
@@ -19,28 +19,24 @@ __forceinline__ __device__ bool operator<(const float3 a, const float3 b)
 }
 
 extern "C" __global__ void __raygen__rg() {
-    // Lookup our location within the launch grid
     const uint3 idx = optixGetLaunchIndex();
 
-    // Map our launch idx to a screen location and create a ray from the camera
-    // location through the screen 
-    float3 ray_origin, ray_direction;
-#if OPTIMIZATION == 0
-    ray_origin    = { float(params.points[idx.x % params.window_size].x), 
-                      float(params.points[idx.x % params.window_size].y),
-                      float(params.points[idx.x % params.window_size].z) };
-#elif OPTIMIZATION == 1
-    ray_origin    = { float(params.ray_origin_list[idx.x].x),
-                      float(params.ray_origin_list[idx.x].y),
-                      float(params.ray_origin_list[idx.x].z) };
-#else
-    ray_origin    = { float(params.points[idx.x].x), 
-                      float(params.points[idx.x].y),
-                      float(params.points[idx.x].z) };
-#endif
-    ray_direction = { 1, 0, 0 };
+    float3 ray_origin;
+    if constexpr (OPTIMIZATION == 0) {
+        ray_origin = { float(params.points[idx.x % params.window_size].x),
+                       float(params.points[idx.x % params.window_size].y),
+                       float(params.points[idx.x % params.window_size].z) };
+    } else if constexpr (OPTIMIZATION == 1) {
+        ray_origin = { float(params.ray_origin_list[idx.x].x),
+                       float(params.ray_origin_list[idx.x].y),
+                       float(params.ray_origin_list[idx.x].z) };
+    } else {
+        ray_origin = { float(params.points[idx.x].x),
+                       float(params.points[idx.x].y),
+                       float(params.points[idx.x].z) };
+    }
+    float3 ray_direction = { 1, 0, 0 };
 
-    // Trace the ray against our scene hierarchy
     unsigned int intersection_test_num = 0;
     unsigned int hit_num = 0;
     unsigned int ray_idx = idx.x % params.window_size;
@@ -49,153 +45,83 @@ extern "C" __global__ void __raygen__rg() {
             ray_origin,
             ray_direction,
             params.tmin,                   // Min intersection distance
-            params.tmax,        // Max intersection distance
-            0.0f,                   // rayTime -- used for motion blur
-            OptixVisibilityMask( 255 ), // Specify always visible
+            params.tmax,                   // Max intersection distance
+            0.0f,                          // rayTime -- used for motion blur
+            OptixVisibilityMask( 255 ),    // Specify always visible
             OPTIX_RAY_FLAG_NONE,
-            0,                   // SBT offset   -- See SBT discussion
-            1,                   // SBT stride   -- See SBT discussion
-            0,                   // missSBTIndex -- See SBT discussion
+            0,                             // SBT offset   -- See SBT discussion
+            1,                             // SBT stride   -- See SBT discussion
+            0,                             // missSBTIndex -- See SBT discussion
             intersection_test_num,
             hit_num,
             ray_idx
             );
-#if OPTIMIZATION == 0 || OPTIMIZATION == 1
-    if (hit_num <= params.K) { // include itself
-        int outlier_idx = atomicAdd(params.outlier_num, 1);
-        params.outlier_list[outlier_idx] = idx.x;
-    }
-#endif
 
-#if DEBUG_INFO == 1
-    params.ray_primitive_hits[idx.x] = hit_num;
-    params.ray_intersections[idx.x]  = intersection_test_num;
-#endif
+    if constexpr (OPTIMIZATION <= 1) {
+        if (hit_num <= params.K) { // include itself
+            int outlier_idx = atomicAdd(params.outlier_num, 1);
+            params.outlier_list[outlier_idx] = idx.x;
+        }
+    }
+
+    if constexpr (DEBUG_INFO == 1) {
+        params.ray_primitive_hits[idx.x] = hit_num;
+        params.ray_intersections[idx.x]  = intersection_test_num;
+    }
 }
 
 extern "C" __global__ void __miss__ms() {
 }
 
-#if OPTIMIZATION == 0
 extern "C" __global__ void __intersection__cube() {
     unsigned int primIdx = optixGetPrimitiveIndex();
-    const double3 point = params.points[primIdx];
-    const double3 ray_orig = params.points[optixGetPayload_2()]; // Get ray origin by its index
-    optixSetPayload_0(optixGetPayload_0() + 1); // number of intersection test
-    bool intersect = false;
-    
-#if DIMENSION == 1
-    if (abs(ray_orig.x - point.x) < params.R) {
-        intersect = true;
+
+    // OPT 2: early exit if this primitive already has enough neighbors
+    if constexpr (OPTIMIZATION == 2) {
+        if (params.outlier_neighbor_num[primIdx] > params.K) return;
     }
-#elif DIMENSION == 3
-    // * pre AABB check 
-    // bool aabb_intersect = false;
-    // float3 topRight = point + params.R;
-    // float3 bottomLeft = point - params.R;
-    // if ((ray_orig > bottomLeft) && (ray_orig < topRight)) {
-    //     aabb_intersect = true;
-    // }
-    // if (aabb_intersect) {
-        // * exact sphere check
+
+    // point  = the BVH primitive   (what we test against)
+    // ray_orig = the query point    (the ray's origin)
+    double3 point, ray_orig;
+    if constexpr (OPTIMIZATION == 2) {
+        point    = params.ray_origin_list[primIdx];
+        ray_orig = params.points[optixGetPayload_2()];
+    } else if constexpr (OPTIMIZATION == 1) {
+        point    = params.points[primIdx];
+        ray_orig = params.ray_origin_list[optixGetPayload_2()];
+    } else {
+        point    = params.points[primIdx];
+        ray_orig = params.points[optixGetPayload_2()];
+    }
+
+    if constexpr (DEBUG_INFO == 1) {
+        optixSetPayload_0(optixGetPayload_0() + 1); // number of intersection test
+    }
+
+    bool intersect = false;
+    if constexpr (DIMENSION == 1) {
+        if (abs(ray_orig.x - point.x) < params.R) intersect = true;
+    } else if constexpr (DIMENSION == 3) {
         double3 O = { ray_orig.x - point.x, ray_orig.y - point.y, ray_orig.z - point.z };
         double sqdist = O.x * O.x + O.y * O.y + O.z * O.z;
-        if (sqdist < params.R2) {
-            intersect = true;
-        }
-    // }
-#endif
+        if (sqdist < params.R2) intersect = true;
+    }
 
     if (intersect) {
-        optixSetPayload_1(optixGetPayload_1() + 1);
-        if (optixGetPayload_1() > params.K) {
-            optixReportIntersection(0, 0);
+        if constexpr (OPTIMIZATION <= 1) {
+            optixSetPayload_1(optixGetPayload_1() + 1);
+            if (optixGetPayload_1() > params.K) {
+                optixReportIntersection(0, 0);
+            }
+        } else {
+            if constexpr (DEBUG_INFO == 1) {
+                optixSetPayload_1(optixGetPayload_1() + 1);
+            }
+            atomicAdd(params.outlier_neighbor_num + primIdx, 1);
         }
     }
 }
-#endif
-
-#if OPTIMIZATION == 1
-extern "C" __global__ void __intersection__cube() {
-    unsigned int primIdx = optixGetPrimitiveIndex();
-    const double3 point = params.points[primIdx];
-    const double3 ray_orig = params.ray_origin_list[optixGetPayload_2()]; // Get ray origin by its index
-    optixSetPayload_0(optixGetPayload_0() + 1); // number of intersection test
-    bool intersect = false;
-    
-#if DIMENSION == 1
-    if (abs(ray_orig.x - point.x) < params.R) {
-        intersect = true;
-    }
-#elif DIMENSION == 3
-    // * pre AABB check 
-    // bool aabb_intersect = false;
-    // float3 topRight = point + params.R;
-    // float3 bottomLeft = point - params.R;
-    // if ((ray_orig > bottomLeft) && (ray_orig < topRight)) {
-    //     aabb_intersect = true;
-    // }
-    // if (aabb_intersect) {
-        // * exact sphere check
-        double3 O = { ray_orig.x - point.x, ray_orig.y - point.y, ray_orig.z - point.z };
-        double sqdist = O.x * O.x + O.y * O.y + O.z * O.z;
-        if (sqdist < params.R2) {
-            intersect = true;
-        }
-    // }
-#endif
-
-    if (intersect) {
-        optixSetPayload_1(optixGetPayload_1() + 1);
-        if (optixGetPayload_1() > params.K) {
-            optixReportIntersection(0, 0);
-        }
-    }
-}
-#endif
-
-#if OPTIMIZATION == 2
-extern "C" __global__ void __intersection__cube() {
-    unsigned int primIdx = optixGetPrimitiveIndex();
-    if (params.outlier_neighbor_num[primIdx] > params.K) return;
-    const double3 point = params.ray_origin_list[primIdx];
-    const double3 ray_orig = params.points[optixGetPayload_2()]; // Get ray origin by its index
-#if DEBUG_INFO == 1
-    optixSetPayload_0(optixGetPayload_0() + 1); // number of intersection test
-#endif
-
-    bool intersect = false;
-    
-#if DIMENSION == 1
-    if (abs(ray_orig.x - point.x) < params.R) {
-        intersect = true;
-    }
-#elif DIMENSION == 3
-    // * pre AABB check -> no optimization
-    // bool aabb_intersect = false;
-    // float3 topRight = point + params.R;
-    // float3 bottomLeft = point - params.R;
-    // if ((ray_orig > bottomLeft) && (ray_orig < topRight)) {
-    //     aabb_intersect = true;
-    // }
-    // if (aabb_intersect) {
-        // * exact sphere check
-        double3 O = { ray_orig.x - point.x, ray_orig.y - point.y, ray_orig.z - point.z };
-        double sqdist = O.x * O.x + O.y * O.y + O.z * O.z;
-        if (sqdist < params.R2) {
-            intersect = true;
-        }
-    // }
-#endif
-
-    if (intersect) {
-#if DEBUG_INFO == 1
-        optixSetPayload_1(optixGetPayload_1() + 1);
-#endif
-        atomicAdd(params.outlier_neighbor_num + primIdx, 1);
-    }
-}
-#endif
 
 extern "C" __global__ void __anyhit__terminate_ray() {
     optixTerminateRay();
